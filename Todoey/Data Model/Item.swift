9b2ebe3bf844c4c525c27ce839aa0b18c741b7bf
Liang Yang <brightsheep@gmail.com>
2018-04-20 05:14:56 -0500

    convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  iem.swift
+//  Todoey
+//
+//  Created by Liang Yang on 4/19/18.
+//  Copyright © 2018 Sydney's Card Lounge. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
